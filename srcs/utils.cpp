00d35dc7566e260604eb4768334a2a45da64db47
Greyback Wynton <gwynton@su-j3.msk.21-school.ru>
2020-11-26 22:31:25 +0300

    fixed split

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: gwynton <gwynton@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 17:41:24 by gwynton           #+#    #+#             */
-/*   Updated: 2020/11/26 21:59:52 by gwynton          ###   ########.fr       */
+/*   Updated: 2020/11/26 22:29:47 by gwynton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 std::vector<std::string> strsplit(const std::string& input, char del)
 {
     std::vector<std::string> result;
-	std::istringstream   strstream(input);
+	std::string temp = input.substr(0, input.size() - 1);
+	std::istringstream   strstream(temp);
 	std::string	buffer;
 
 	while (std::getline(strstream, buffer, del))
+	{
 		result.push_back(buffer);
+	}
 	return (result);
 }
