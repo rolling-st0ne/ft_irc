d55547f0477905cd09dd151c63d3a8a36f1ffa50
Alexander Shust <ashust@mail.ru>
2020-12-04 09:08:04 +0300

    NICK shenanigans

--- srcs/commands/nick.cpp
+++ srcs/commands/nick.cpp
@@ -6,7 +6,7 @@
 /*   By: gwynton <gwynton@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 18:10:11 by casteria          #+#    #+#             */
-/*   Updated: 2020/12/04 04:34:50 by gwynton          ###   ########.fr       */
+/*   Updated: 2020/12/04 09:04:55 by gwynton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,33 @@ void        IrcAPI::cmd_nick(Server& server, Client& client, const t_command& co
 		throw IrcException(ERR_NICKNAMEINUSE);
 	if (!client.is_registered && nick_in_use(server, command.params[0]))
 		throw IrcException(ERR_NICKCOLLISION);
+	std::string old_nick = client.name;
 	client.name = command.params[0]; // length of name should be 9 symbols
+	//std::cerr << "Old nick: " << old_nick << std::endl;
+	//if (client.is_registered) // This gets reset somehow, I'll investigate
+	{
+		std::string message = user_by_nick(server, old_nick) + " NICK " + client.name;
+		for (size_t i = 0; i < server.users.size(); i++)
+		{
+			//std::cerr << "Checking nick: " << server.users[i].nickname << std::endl;
+			if (server.users[i].nickname == old_nick)
+			{
+				//std::cerr << "Changing " << old_nick << " to " << client.name << std::endl;
+				server.users[i].nickname = client.name;
+				break;
+			}
+		}
+		for (size_t i = 0; i < server.users.size(); i++)
+		{
+			sendToUser(server, server.users[i].nickname, message);
+		}
+		for (size_t i = 0; i < server.channels.size(); i++)
+		{
+			for (size_t j = 0; j < server.channels[i].members.size(); j++)
+			{
+				if (server.channels[i].members[j] == old_nick)
+					server.channels[i].members[j] = client.name;
+			}
+		}
+	}
 }
